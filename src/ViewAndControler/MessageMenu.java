59eec8729dae96146a4af0fa1fd85f6b2a4f4ec7
tala <metala82@gmail.com>
2023-03-19 01:44:26 +0330

    fixing the problem in enterCommand

--- src/ViewAndControler/MessageMenu.java
+++ src/ViewAndControler/MessageMenu.java
@@ -66,7 +66,7 @@ public class MessageMenu {
                 if (name.equalsIgnoreCase("private chat")) {
                     name = "privateChat";
                 }
-                for (int i = 0; i < currentUser.getChats().size() - 1; i++) {
+                for (int i = 0; i < currentUser.getChats().size(); i++) {
                     Chat currentUserChat = currentUser.getChats().get(i);
                     if (currentUserChat.getClass().getSimpleName().equalsIgnoreCase(name)) {
                         if (currentUserChat.getId().equals(id)) {
